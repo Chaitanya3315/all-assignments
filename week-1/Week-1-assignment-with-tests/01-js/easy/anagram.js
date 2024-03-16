0b130a3a1e08426219390fc3c08044eb431ea90f
Chaitanya3315 <tkrishnachaitanya741@gmail.com>
2024-03-16 17:09:49 +0530

    anagram solved

--- week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
@@ -8,7 +8,14 @@
 */
 
 function isAnagram(str1, str2) {
-
+  let st1 = Array.from(str1.toLowerCase()).sort();
+  let st2 = Array.from(str2.toLowerCase()).sort();
+  console.log(st1,st2);
+  if(st1.length != st2.length){
+    return false;
+  }else{
+    return st1.toString() == st2.toString() ? true : false;
+  }
 }
 
 module.exports = isAnagram;
