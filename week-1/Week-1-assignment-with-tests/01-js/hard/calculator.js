e04067592bcea0f5084122b32ecc8830c475aed8
Chaitanya3315 <tkrishnachaitanya741@gmail.com>
2024-03-16 22:25:00 +0530

    calculator done using eval function

--- week-1/Week-1-assignment-with-tests/01-js/hard/calculator.js
+++ week-1/Week-1-assignment-with-tests/01-js/hard/calculator.js
@@ -14,9 +14,53 @@
         2. the input can have invalid non-numerical characters like `5 + abc`, you're supposed to throw error for such inputs
 
   Once you've implemented the logic, test your code by running
-  - `npm run test-calculator`
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.result = 0;
+  }
+  add(num) {
+    if (typeof num === "number") {
+      this.result += num;
+    } else {
+      throw new Error("Invalid number");
+    }
+  }
+  subtract(num) {
+    if (typeof num === "number") {
+      this.result -= num;
+    } else {
+      throw new Error("Invalid number");
+    }
+  }
+  multiply(num) {
+    if (typeof num === "number") {
+      this.result *= num;
+    } else {
+      throw new Error("Invalid number");
+    }
+  }
+  divide(num) {
+    if (typeof num === "number" && num !== 0) {
+      this.result /= num;
+    } else {
+      throw new Error("Invalid number");
+    }
+  }
+  clear() {
+    this.result = 0;
+  }
+  getResult() {
+    return this.result;
+  }
+  calculate(expression) {
+    this.result = eval(expression.replace(/\s+/g, " "))
+    if(this.result === Infinity) {
+      throw new Error("Infinity")
+    }
+    return this.result;
+  }
+}
 
 module.exports = Calculator;
