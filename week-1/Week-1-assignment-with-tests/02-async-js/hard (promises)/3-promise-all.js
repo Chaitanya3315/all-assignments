4efc34ea48405cd04a70967dbd1eda9f74fd655d
Chaitanya3315 <tkrishnachaitanya741@gmail.com>
2024-03-18 11:01:00 +0530

    promise all done

--- week-1/Week-1-assignment-with-tests/02-async-js/hard (promises)/3-promise-all.js
+++ week-1/Week-1-assignment-with-tests/02-async-js/hard (promises)/3-promise-all.js
@@ -6,17 +6,42 @@
 
 
 function waitOneSecond() {
-
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve("Wait for 1 sec Done");
+        },1000)
+    });
 }
 
 function waitTwoSecond() {
-
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve("Wait for 2 sec Done");
+        },2000)
+    });
 }
 
 function waitThreeSecond() {
-
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve("Wait for 3 sec Done");
+        },3000)
+    });
 }
 
 function calculateTime() {
+    const start = new Date().getTime();
+    return Promise.all([waitOneSecond(), waitTwoSecond(), waitThreeSecond()]).then((values) => {
+        const end = new Date().getTime();
+        console.log("Time taken :", end -start);
+        return values;
+      });
+   
 
 }
+
+calculateTime().then((result)=>{
+    console.log(result);
+}).catch((error)=>{
+    console.log(error);
+});
