047fd42d0bd5aca15bd5febc005604a271780d94
Chaitanya3315 <tkrishnachaitanya741@gmail.com>
2024-03-18 11:06:28 +0530

    promisechain done

--- week-1/Week-1-assignment-with-tests/02-async-js/hard (promises)/4-promise-chain.js
+++ week-1/Week-1-assignment-with-tests/02-async-js/hard (promises)/4-promise-chain.js
@@ -6,17 +6,68 @@
  */
 
 function waitOneSecond() {
-
-}
-
-function waitTwoSecond() {
-
-}
-
-function waitThreeSecond() {
-
-}
-
-function calculateTime() {
-
-}
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Wait for 1 sec Done");
+        }, 1000);
+    });
+  }
+  
+  function waitTwoSecond() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Wait for 2 sec Done");
+        }, 2000);
+    });
+  }
+  
+  function waitThreeSecond() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Wait for 3 sec Done");
+        }, 3000);
+    });
+  }
+  
+  function sequentialExecution() {
+    const startTime = Date.now();
+    return waitOneSecond()
+        .then(result1 => {
+            console.log(result1);
+            return waitTwoSecond();
+        })
+        .then(result2 => {
+            console.log(result2);
+            return waitThreeSecond();
+        })
+        .then(result3 => {
+            console.log(result3);
+            const endTime = Date.now();
+            console.log("Total time taken:", endTime - startTime, "milliseconds");
+        });
+  }
+  
+  sequentialExecution();
+  
+  async function sequentialExecution() {
+    const startTime = Date.now();
+  
+    try {
+        const result1 = await waitOneSecond();
+        console.log(result1);
+  
+        const result2 = await waitTwoSecond();
+        console.log(result2);
+  
+        const result3 = await waitThreeSecond();
+        console.log(result3);
+  
+        const endTime = Date.now();
+        console.log("Total time taken:", endTime - startTime, "milliseconds");
+    } catch (error) {
+        console.error("Error:", error);
+    }
+  }
+  
+  sequentialExecution();
+  
